1fe9911bbc1139b83b338c00b88dbb6aec030edf
logonrm <logonrm@fiap.com.br>
2018-05-12 16:20:41 -0300

    ionic project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,44 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { DespesaProvider } from '../providers/despesa/despesa';
+import { AddPage } from '../pages/add/add';
+/* LocalStorage */
+import { LocalStorageModule } from 'angular-2-local-storage';
+import { CloudSettings, CloudModule } from '@ionic/cloud-angular';
+
+const cloudSettings: CloudSettings = {
+  'core': {
+    'app_id': '81d1d2f1'
+  }
+};
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AddPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    LocalStorageModule.withConfig({
+      prefix: 'despesasApp',
+      storageType: 'localStorage'
+    })//,
+  //  CloudModule.forRoot(cloudSettings)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AddPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    DespesaProvider
   ]
 })
 export class AppModule {}
